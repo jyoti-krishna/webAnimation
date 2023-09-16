4909c90cce571a676dd7272d5f9e5d87cc5fd579
jyoti-krishna <jyotikrishnabehera11@gamil.com>
2023-09-16 17:18:42 +0530

    first

---
+++ script.js
@@ -0,0 +1,16 @@
+gsap.to('.imagecontainer',{
+    width: "100%",
+    ease: Expo.easeInOut,
+    stagger:2
+})
+gsap.to(".text h1",{
+    ease: Expo.easeInOut,
+    stagger:2,
+    top:0
+})
+gsap.to(".text h1",{
+    delay:2,
+    ease: Expo.easeInOut,
+    stagger:2,
+    top:"-100%"
+})
